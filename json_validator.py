24402659bde916db18445c528aa514ccae850547
Andrea <andybbruno@gmail.com>
2019-05-09 16:32:39 +0200

    debug

--- json_validator.py
+++ json_validator.py
@@ -1,7 +1,8 @@
 from schema import Schema, And, Use
 
 mylist = ('caffe', 'cioccolato')
+
 new_machine = Schema({
-                    'ID':       Use(lambda x: len(x) != 10),
+                    'ID':       Use(int)
                     'orders':   Use(lambda s: any([True if x.lower in mylist else False for x in s ])),
                     }, ignore_extra_keys = True)
