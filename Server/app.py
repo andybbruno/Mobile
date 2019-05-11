50ee3aaaa13a292fc1cc1701bea4debad4d519ac
Andrea <andybbruno@gmail.com>
2019-05-11 15:31:23 +0200

    Registration completed

--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, render_template, redirect, session
-import pymongo, os
+from flask import Flask, request, render_template, redirect, session, url_for
+import pymongo
+import requests
+import os
+import json
 from datetime import datetime as data
 from random import randint
 from json_validator import Validator
@@ -16,12 +19,41 @@ userTable = mobile_db["testUser"]
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
+
 @app.route('/')
 def homepage():
-    if ('username' in session):
-        return render_template('index.html', username=session['username'])
+    if ('username' in session) and ('logged' in session):
+        return render_template('index.html',
+                               username=session['username'],
+
+                               )
     else:
         return redirect('/login')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        if userTable.find_one({"username": username}):
+            return render_template('register.html', error='Username already in use!')
+        elif password1 != password2:
+            return render_template('register.html', error="Please check the passwords")
+        else:
+            userTable.insert_one({"username": username, "password": password1})
+            session['username'] = username
+            return redirect('/reg')
+    else:
+        return render_template('register.html')
+
+
+@app.route('/reg', methods=['GET'])
+def reg_complete():
+    print(session['username'])
+    return render_template('regcomplete.html', user=session['username'])
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -32,6 +64,7 @@ def login():
 
         if userTable.find_one({"username": username, "password": password}):
             session['username'] = username
+            session['logged'] = True
             return redirect('/')
         else:
             return render_template('login.html', error="Incorrect username or password")
@@ -42,6 +75,7 @@ def login():
 def genereteID():
     n = 5
     return randint(10**(n-1), (10**n)-1)
+
 
 @app.route('/machine', methods=['POST'])
 def new_machine():
@@ -72,14 +106,14 @@ def new_machine():
     if machineTable.find_one({"ID": ID}):
         return "Machine already exist"
 
-    currTime = (int) (data.timestamp(data.now()))
+    currTime = (int)(data.timestamp(data.now()))
     machine = {
         "ID": ID,
         "possible_orders": jsonReq["orders"],
         "position_geo": jsonReq.get("position_geo", None),
         "position_des": jsonReq["position_des"],
         "maintenance": {
-            "consumable_list":{
+            "consumable_list": {
             },
             "last_maintenance": currTime,
             "last_cleaning": currTime,
@@ -106,6 +140,7 @@ def new_machine():
     print("New", machine)
     machineTable.insert_one(machine)
     return 'New machine Adder'
+
 
 @app.route('/machine', methods=['DELETE'])
 def del_machine():
@@ -161,7 +196,7 @@ def new_order(ID):
             TRUE se la rigistrazione è avventua FALSE altrimenti
     """
     jsonReq = request.get_json(silent=True, force=True)
-    timestamp = (int) (data.timestamp(data.now()))
+    timestamp = (int)(data.timestamp(data.now()))
     currMachine = machineTable.find_one({"ID": ID})
     if not currMachine:
         return "Not Valid Machine"
@@ -169,7 +204,7 @@ def new_order(ID):
     if not Validator.validate_order(jsonReq, currMachine["possible_orders"].keys()):
         return "Not Valid JSON"
 
-    #Inserimento transazione
+    # Inserimento transazione
     cost = currMachine["possible_orders"][jsonReq["product"]]
     transactionTable.insert_one({
         "timestamp": timestamp,
@@ -179,17 +214,19 @@ def new_order(ID):
         "cost": cost,
         "satisfaction": jsonReq["satisfaction"]
     })
-    #Inserimento della people detection durante l'acquisto
+    # Inserimento della people detection durante l'acquisto
     detectionTable.insert_one({
         "timestamp": timestamp,
         "machineID": ID,
         "people_detected": jsonReq["people_detected"]
     })
     # aggiorna il numero di vendite sulla macchnetta
-    machineTable.update_one({"ID": ID}, {"$inc":{"management.count_orders."+jsonReq["product"]: 1}})
+    machineTable.update_one(
+        {"ID": ID}, {"$inc": {"management.count_orders."+jsonReq["product"]: 1}})
     # modifica satisfaction_level della macchinetta (media armonica)
     currSat, newSat = currMachine["satisfaction_level"], jsonReq["satisfaction"]
-    machineTable.update_one({"ID": ID}, {"$set":{"satisfaction_level": (2*currSat*newSat)/(newSat+currSat)}})
+    machineTable.update_one(
+        {"ID": ID}, {"$set": {"satisfaction_level": (2*currSat*newSat)/(newSat+currSat)}})
     # modifico i livelli degli oggetti di consumo che hanno avuto ua variazione
     if jsonReq.get("new_levels", None):
         to_modify = {}
@@ -197,6 +234,7 @@ def new_order(ID):
             to_modify = {"maintenance.consumable_list."+k: v}
         machineTable.update_one({"ID": ID}, {"$set": to_modify})
     return "Transaction registered"
+
 
 @app.route('/<int:ID>/mngt', methods=['POST', 'GET'])
 def manage():
@@ -212,7 +250,7 @@ def manage():
 @app.route('/all', methods=['GET', "DELETE"])
 def allData():
     def readTable(table):
-        curso = machineTable.find()
+        cursor = machineTable.find()
         list = []
         for document in cursor:
             list.append(document)
@@ -224,18 +262,20 @@ def allData():
     listUser = readTable(userTable)
 
     if request.method == "DELETE":
-        machineTable.delete_many(listMachine)
-        transactionTable.delete_many(listTransaction)
-        detectionTable.delete_many(listDetection)
-        userTable.delete_many(listUser)
+        userTable.drop
+        machineTable.drop
+        transactionTable.drop
+        detectionTable.drop
 
     return "Machine\n "+listMachine+" \n\n Transaction\n "+listTransaction+" \n\n Detection\n "+listDetection+" \n\nUser\n "+listUser+" \n\n"
 
-#IDEA: bot telegram
+# IDEA: bot telegram
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
+
 
 app.run(host='127.0.0.1', port='5000', debug=True)
