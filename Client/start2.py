b8309c12235c7d64243c0792225ee684ff99bcf1
Andrea <andybbruno@gmail.com>
2019-05-14 23:16:03 +0200

    Fixed width and color

--- Client/start2.py
+++ Client/start2.py
@@ -95,10 +95,10 @@ try:
         for obj in parsed['objects']:
             if (obj['object'] == 'person'):
                 people += 1
-                draw.rectangle(getRectangle(obj), outline='red', width=2)
+                draw.rectangle(getRectangle(obj), outline='rgb(255,0,0)', width=2)
 
         for face in parsed['faces']:
-            draw.rectangle(getRectangle(face), outline='green', width=2)
+            draw.rectangle(getRectangle(face), outline='rgb(0,255,0)', width=2)
 
         
         elapsed_time = time.time() - start_time
