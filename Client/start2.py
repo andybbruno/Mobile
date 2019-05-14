ef4d8ec149956d3f922e1fcb9364ec46c54acccb
Andrea <andybbruno@gmail.com>
2019-05-14 22:16:05 +0200

    test

--- Client/start2.py
+++ Client/start2.py
@@ -96,74 +96,7 @@ try:
                                  params=params,
                                  data=frame)
 
-        if response.status_code != 200:
-            raise Exception("Error:", response)
-
-        parsed = response.json()
-
-        people = 0
-        faces = len(parsed['faces'])
-
-        for obj in parsed['objects']:
-            if (obj['object'] == 'person'):
-                people += 1
-                x, y, w, h = getRectangle(obj)
-                # cv2.rectangle(frame, (x, y), (w, h), (0, 255, 0), 2)
-
-        for face in parsed['faces']:
-            x, y, w, h = getRectangle(face)
-            # cv2.rectangle(frame, (x, y), (w, h), (0, 0, 255), 2)
-
-        if(debug):
-            elapsed_time = time.time() - start_time
-            print("<", people, " people, ", faces,
-                  " faces> ", elapsed_time, " seconds")
-            if(ssh):
-                img2 = Image.fromarray(frame, 'RGB')
-                b, g, r = img2.split()
-                img2 = Image.merge("RGB", (r, g, b))
-                img2.show()
-            # else:
-                # cv2.imshow('frame', frame)
-                # cv2.waitKey(30)
-
-        else:
-            elapsed_time = time.time() - start_time
-
-            print("<", people, " people, ", faces,
-                  "faces> ", elapsed_time, " seconds")
-
-            url_ord = ec2 + '/' + str(ID) + '/order'
-
-            trn = trans[random.randint(1, len(trans) - 1)]
-            prd = prod[random.randint(1, len(prod) - 1)]
-
-            # TODO: add products levels
-            tmp = {"transaction_type": trn,
-                   "product": prd,
-                   "satisfaction": random.random(),
-                   "people_detected": people,
-                   "face_recognised": faces
-                   }
-
-            try:
-                requests.post(url_ord, json=json.dumps(tmp))
-            except Exception as e:
-                print('Error:' , e)
-
-
-            url_frame = ec2 + '/' + str(ID) + '/live'
-            path = str(ID) + ".jpg"
-            # res = cv2.resize(frame, (640,360)) 
-            # cv2.imwrite(path,res)
-            
-            png.from_array(frame).save(path)
-            
-            with open(path, 'rb') as f:
-                try:
-                    requests.post(url_frame, files={"frame": f})
-                except Exception as e:
-                    print('Error:' , e)
+        print(response.content)
 
 except Exception as e:
     print('Error:' , e)
