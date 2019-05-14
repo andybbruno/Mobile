e85481df9199457706507ad67e97fdf43099af0b
Andrea <andybbruno@gmail.com>
2019-05-14 21:43:22 +0200

    Test

--- Client/start.py
+++ Client/start.py
@@ -75,9 +75,9 @@ try:
 
         rawCapture = PiRGBArray(camera)
         camera.capture(rawCapture, format="bgr")
-        frame = rawCapture.array.tobytes()
+        frame = rawCapture.array
 
-        print("FRAME --> " , str(type(frame)))
+        print("FRAME --> " , str(type(frame.tobytes())))
         # ~ frame = cv2.flip(frame, -1)
 
         img_str = cv2.imencode('.jpg', frame)[1].tostring()
