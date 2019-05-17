b27fc057196cece8ad2351062cebcdaa2393c749
Andrea <andybbruno@gmail.com>
2019-05-17 11:54:42 +0200

    Fixed 404 and searchbar

--- Server/app.py
+++ Server/app.py
@@ -389,8 +389,8 @@ def addUser():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html')
+     # note that we set the 404 status explicitly
+    return renderWith(render_template("main-panel/not_found.html"))
 
 
 app.run(host='0.0.0.0', port='3000', debug=True)
