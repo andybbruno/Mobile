177f7c23e6d2048331190ca83423e9e7ed847657
Andrea <andybbruno@gmail.com>
2019-05-20 14:57:07 +0200

    Added work for Rino

--- Simulator/simulate.py
+++ Simulator/simulate.py
@@ -1,64 +1,86 @@
+from flask import Flask, request, render_template
+import os, requests, json
 
-"""
-Script per la simulazione del di un interazione intensa tra utenti e macchinetta
-"""
-server = None #TODO creare il collegameno al vero server
-#TODO simulare anche il collegameto con il rasp per la richesta dei livelli
-
-#Cancello tutto quello che c'è nel database per partire da zero
-server.delete("/all")
-# Ripopolo il Database con dati fittizi
-
-server.post("/addUser", json = {})
-
-#Creo gli operatori
-server.post("/addUser", json = {
-    "ID": "1",
-    "role": "operator"
-    "username": "Fesso"
-    "password": "pw"
-})
-
-#Creo le macchinette
-server.post("/machine", json = {
-"ID": "111"
-"orders": {"caffe": 0.8, "cioccolato": 1},
-"position_des": "Infondo a destra c'è il cesso!",
-"owner": "TuaMa srl",
-"consumable_list": ["bicchiere", "palettina", "caffe", "cioccolato concentrato", "zucchero"],
-"operatorID": "Oper_1"
-})
-
-server.post("/machine", json = {
-"ID": "222"
-"orders": {"caffe": 0.6, "cioccolato": 1.1, "te": 0.7, "bicchere vuoto": 0.2},
-"position_des": "Infondo a destra c'è il cesso!",
-"owner": "TuaMa srl",
-"consumable_list": ["bicchiere", "palettina", "caffe", "cioccolato concentrato", "zucchero"],
-"operatorID": "Oper_1"
-})
-
-#simulo lapulizia e il refill
-server.post("/111/maintenance", json = {
-    "operatorID": 1,
-    "type": "cleaning"
-})
-server.post("/111/maintenance", json = {
-    "operatorID": 1,
-    "type": "refill"
-})
-server.post("/222/maintenance", json = {
-    "operatorID": 1,
-    "type": "cleaning"
-})
-server.post("/222/maintenance", json = {
-    "operatorID": 1,
-    "type": "refill"
-})
-
-#simulo degli ordini fatti sulle due macchinette
-server.post("/", json = {})
-server.post("/addUser", json = {})
+app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
 
-server.post("/challenge", data={"runs":[1]}, follow_redirects=True)
+
+@app.route('/', methods=['GET'])
+def homepage():
+    return render_template("simulator.html")
+
+
+@app.route('/', methods=['POST'])
+def order():
+
+
+    url = "http://ec2-18-212-110-170.compute-1.amazonaws.com:3000/11222/order"
+
+
+    lista = ("caffe" "cappuccino", "te", "cioccolato")
+    val = ""
+    man_type = ""
+
+    order = False
+    maint = False
+
+    if (request.form['Caffe'] is not None):
+        val = "caffe"
+        order = True
+
+    if (request.form['Cioccolata'] is not None):
+        val = "cioccolata"
+        order = True
+
+    if (request.form['Cappuccino'] is not None):
+        val = "cappuccino"
+        order = True
+    
+    if (request.form['Te'] is not None):
+        val = "te"
+        order = True
+
+    if (request.form['manutenzione'] is not None):
+        man_type = "repair"
+        maint = True
+
+    if (request.form['refill'] is not None):
+        man_type = "refill"
+        maint = True
+
+    if (request.form['fine'] is not None):
+        # TODO: RINO FAI QUESTA COSA 
+        man_type = "fine"
+        maint = True
+
+    if (request.form['manutenzione'] is not None):
+        # TODO: RINO FAI QUESTA COSA 
+        man_type = "repair"
+        maint = True
+
+    
+
+    if (order):
+        data = {"transaction_type": "cash",
+            "product": val,
+            "satisfaction": 0.99,
+            "people_detected": 99,
+            "face_recognised": 99
+            }
+        print(requests.post(url, json=json.dumps(data)))
+        return render_template("simulator.html")
+
+    if maint:
+        data = {
+            "operatorID" : 12345
+            "type" : man_type
+        }
+
+        return render_template("simulator.html")
+
+
+
+
+
+app.run(host='0.0.0.0', port='4000', debug=True)
