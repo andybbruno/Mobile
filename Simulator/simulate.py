babdd71bc277531c6cffced65602cdd268cbcb9e
Ruggiero-Santo <ruggiero.santo@gmail.com>
2019-05-20 15:45:19 +0200

    Ho fatto il lavoro hce dovevo

--- Simulator/simulate.py
+++ Simulator/simulate.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, render_template
 import os, requests, json
+from datetime import datetime as time
+from random import randint
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
+
+import pymongo
+
+_mongoDB = pymongo.MongoClient("mongodb://ec2-18-212-110-170.compute-1.amazonaws.com:27017/")
+_mobile_db = _mongoDB["test_db"]
+
+machineTable = _mobile_db["testTable"]
+detectionTable = _mobile_db["testDetection"]
 
 
 
@@ -14,8 +24,8 @@ def homepage():
 @app.route('/', methods=['POST'])
 def order():
 
-
-    url = "http://ec2-18-212-110-170.compute-1.amazonaws.com:3000/11222/order"
+    id = 33444
+    url = "http://ec2-18-212-110-170.compute-1.amazonaws.com:3000/"+id+"/"
 
 
     lista = ("caffe" "cappuccino", "te", "cioccolato")
@@ -25,6 +35,7 @@ def order():
     order = False
     maint = False
 
+    #--------------------------------------------Ordini------------------------- 
     if (request.form['Caffe'] is not None):
         val = "caffe"
         order = True
@@ -41,25 +52,48 @@ def order():
         val = "te"
         order = True
 
+    
+    #--------------------------------------------Manutenzione------------------------- 
     if (request.form['manutenzione'] is not None):
+        #modifica data di manutenzione della macchina
+        machineTable.update_one({"ID": id}, { "$set": { "maintenance.last_maintenance": time.timestamp(time.now()) }})
         man_type = "repair"
         maint = True
 
     if (request.form['refill'] is not None):
+        # modifica valori dell 
+        machineTable.update_one({"ID": id}, { "$set": { "maintenance.consumable_list":{
+            "bicchiere": 20 + randint(-10, 10),
+            "palettina": 20 + randint(-10, 10),
+            "caffe": 20 + randint(-10, 10),
+            "zucchero": 20 + randint(-10, 10),
+            "latte": 20 + randint(-10, 10),
+            "te": 20 + randint(-10, 10),
+            "cioccolato": 45
+        }}})
         man_type = "refill"
         maint = True
 
     if (request.form['fine'] is not None):
-        # TODO: RINO FAI QUESTA COSA 
-        man_type = "fine"
+        # simulazione della fine consumabili
+        machineTable.update_one({"ID": id}, { "$set": { "maintenance.consumable_list":{
+            "bicchiere": 100,
+            "palettina": 100,
+            "caffe": 100,
+            "zucchero": 100,
+            "latte": 100,
+            "te": 100,
+            "cioccolato": 100
+        }}})
+        man_type = None
         maint = True
 
-    if (request.form['manutenzione'] is not None):
-        # TODO: RINO FAI QUESTA COSA 
-        man_type = "repair"
+    if (request.form['rottura'] is not None):
+        # registrazione della raottura
+        machineTable.update_one({"ID": id}, { "$set": { "working": False } })
+        man_type = None
         maint = True
 
-    
 
     if (order):
         data = {"transaction_type": "cash",
@@ -68,19 +102,18 @@ def order():
             "people_detected": 99,
             "face_recognised": 99
             }
-        print(requests.post(url, json=json.dumps(data)))
+        print(requests.post(url+"order", json=json.dumps(data)))
         return render_template("simulator.html")
 
     if maint:
-        data = {
-            "operatorID" : 12345,
-            "type" : man_type
-        }
+        if man_type is not None:
+            data = {
+                "operatorID" : 12345,
+                "type" : man_type
+            }
 
+            print(requests.post(url+"order", json=json.dumps(data)))
         return render_template("simulator.html")
-
-
-
 
 
 app.run(host='0.0.0.0', port='4000', debug=True)
