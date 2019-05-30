f2bae8f80b4efa49a54d0655d206aa72edf89530
Andrea <andybbruno@gmail.com>
2019-05-30 14:07:05 +0200

    Removed order via Raspberry + useless imports

--- Client/test_no_opencv.py
+++ Client/test_no_opencv.py
@@ -1,9 +1,17 @@
+import requests
+import time
+import json
 from picamera import PiCamera
 from picamera.array import PiRGBArray
-import cv2, time, requests, os
+import os
+import random
+import numpy as np
+from PIL import Image, ImageDraw
+from io import BytesIO
 
 
-looptime = 5
+# Duty cycle
+duty = 3
 
 # Machine ID
 ID = 11222
@@ -40,19 +48,19 @@ def getRectangle(personDict):
             top = rect['y']
             bottom = left + rect['w']
             right = top + rect['h']
-            return left, top, bottom, right
+            return ((left, top), (bottom, right))
     else:
         rect = personDict['faceRectangle']
         left = rect['left']
         top = rect['top']
         bottom = left + rect['height']
         right = top + rect['width']
-        return left, top, bottom, right
+        return ((left, top), (bottom, right))
 
 
 camera = PiCamera()
 camera.resolution = (1280, 720)
-time.sleep(0.1)
+time.sleep(0.5)
 
 elapsed_time = 0
 try:
@@ -61,21 +69,18 @@ try:
         print(elapsed_time)
 
         # Per evitare di fare più di 20 chiamate al min
-        if (elapsed_time < looptime):
-            time.sleep(looptime - elapsed_time)
+        if (elapsed_time < duty):
+            time.sleep(duty - elapsed_time)
 
         start_time = time.time()
 
-        rawCapture = PiRGBArray(camera)
-        camera.capture(rawCapture, format="bgr")
-        frame = rawCapture.array
-
-        img_str = cv2.imencode('.jpg', frame)[1].tostring()
+        frame = np.empty((1280, 720, 3), dtype=np.uint8)
+        camera.capture(frame, format='jpeg')
 
         response = requests.post(vision_analyze_url,
                                  headers=headers,
                                  params=params,
-                                 data=img_str)
+                                 data=frame.tostring())
 
         if response.status_code != 200:
             raise Exception("Error:", response)
@@ -85,33 +90,52 @@ try:
         people = 0
         faces = len(parsed['faces'])
 
+        img = Image.open(BytesIO(frame))
+        draw = ImageDraw.Draw(img)
+
         for obj in parsed['objects']:
             if (obj['object'] == 'person'):
                 people += 1
-                x, y, w, h = getRectangle(obj)
-                cv2.rectangle(frame, (x, y), (w, h), (0, 255, 0), 2)
+                draw.rectangle(getRectangle(
+                    obj), outline='rgb(0,255,0)', width=2)
 
         for face in parsed['faces']:
-            x, y, w, h = getRectangle(face)
-            cv2.rectangle(frame, (x, y), (w, h), (0, 0, 255), 2)
+            draw.rectangle(getRectangle(face), outline='rgb(255,0,0)', width=2)
 
-        
-        
+        elapsed_time = time.time() - start_time
+
         print("<", people, " people, ", faces, "faces> ")
 
+        url_ord = ec2 + '/' + str(ID) + '/order'
+
+        trn = trans[random.randint(1, len(trans) - 1)]
+        prd = prod[random.randint(1, len(prod) - 1)]
+
+        # TODO: add products levels
+        tmp = {"transaction_type": trn,
+               "product": prd,
+               "satisfaction": random.random(),
+               "people_detected": people,
+               "face_recognised": faces
+               }
+
+        try:
+            requests.post(url_ord, json=json.dumps(tmp))
+        except Exception as e:
+            print('Error:', e)
 
         url_frame = ec2 + '/' + str(ID) + '/live'
         path = str(ID) + ".jpg"
-        res = cv2.resize(frame, (640,360)) 
-        cv2.imwrite(path,res)
-                    
+
+        resized = img.resize((640,360), Image.ANTIALIAS)
+        resized.save(path)
+
         with open(path, 'rb') as f:
             try:
                 requests.post(url_frame, files={"frame": f})
             except Exception as e:
-                print('Error:' , e)
-        
-        elapsed_time = time.time() - start_time
+                print('Error:', e)
+
 
 except Exception as e:
-    print('Error:' , e)
+    print('Error:', e)
